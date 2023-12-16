960f4edca4a955981433487b8eac5ede45b0a48c
Josef Sedlačík <Zipik83@gmail.com>
2023-12-17 00:02:40 +0100

    AoC template

---
+++ Templates/AoC/AoC.cs
@@ -0,0 +1,24 @@
+using AdventOfCode.Shared;
+
+namespace AdventOfCode._1970;
+
+public class AoC : Day
+{
+    public static void Main()
+    {
+        var instance = new AoC();
+        Console.WriteLine();
+        Console.WriteLine($"RESULT#1: {instance.GetTask1Result()}");
+        Console.WriteLine($"RESULT#2: {instance.GetTask2Result()}");
+    }
+
+    public override long GetTask1Result(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long GetTask2Result(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
