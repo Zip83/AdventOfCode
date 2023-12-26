8d38ce7580f304f25d61475b6a68c902ceb7e195
Zip <Zipik83@gmail.com>
2023-12-26 18:18:53 +0100

    Year 2022

---
+++ 2022/Day 17/Day 17.cs
@@ -0,0 +1,24 @@
+using AdventOfCode.Shared;
+
+namespace AdventOfCode._2022;
+
+public class Day17 : Day
+{
+    public static void Main()
+    {
+        var instance = new Day17();
+        Console.WriteLine();
+        Console.WriteLine($"RESULT#1: {instance.GetTask1Result()}");
+        Console.WriteLine($"RESULT#2: {instance.GetTask2Result()}");
+    }
+
+    public override long GetTask1Result(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long GetTask2Result(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
